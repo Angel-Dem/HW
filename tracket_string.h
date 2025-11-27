cbb79dd40d79f9159eff877478cd855ef142a777
Angel-Dem <angelinazubkova486@gmail.com>
2025-11-27 19:45:35 +0300

    HWs

--- tracket_string.h
+++ tracket_string.h
@@ -3,14 +3,25 @@
 
 class TrackedString {
     std::string value;
-    static int counter;
+    static int counter; //счетчик создаваемых объктов
     int id;
 public:
+    //конструктор по умолчанию
     TrackedString() : value(""), id(counter) {
+        //в логер передаем какое-то инфо - мэсседж
+        //
         Logger::getInstance().log(
-        Logger::Level::INFO,
             "[default ctor] id=" + std::to_string(id) +
-            " this=" + pointerToString(this)
+            " this=" + pointerToString(this),
+            Logger::Level::INFO
         );
     }
+    //конструктор из std::string;
+    TrackedString(const std::string& str) : value(str), id(counter) {
+        Logger::getInstance().log(
+            "[string ctor] id=" + std::to_string(id)+
+
+        );
+    }
+
 };
